bd9825ede7595594666d83a1aa52edf8ad729c85
gun3486 <gun3486@naver.com>
2021-03-29 21:13:47 +0900

    급식크롤링
    크롤링

--- school.py
+++ school.py
@@ -1,7 +1,12 @@
 import requests
-import json
+from bs4 import BeautifulSoup
+import datetime
 
-url = 'https://schoolmenukr.ml/api/high/F100000120'
-response = requests.get(url)
-school_menu = json.loads(response.text)
-print(school_menu)
+present = str(datetime.datetime.now())
+print(present)
+date = present[:4] + present[5:7] + present[8:10] 
+print(date)
+
+req = requests.get("http://gsm.gen.hs.kr/xboard/board.php?tbnum=8")
+soup = BeautifulSoup(req.text, "html.parser")
+print(soup)
