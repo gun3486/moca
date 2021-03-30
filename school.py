c1b30699e6b0661d7ffae39a8e7aa6408108c64c
gun3486 <gun3486@naver.com>
2021-03-30 20:09:06 +0900

    xml에서json으로변경
    변경

--- school.py
+++ school.py
@@ -1,2 +1,7 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('')
+tree = ET.parse('C:\Users\user\Documents\Github\moca\급식식단정보')
+root = tree.getroot()
+print(root.tag)
+for i in root:
+    print(i.tag, i.text)
+    
