183662c9a883e753d90742679ab2162141877a45
gun3486 <gun3486@naver.com>
2021-03-30 18:26:23 +0900

    xml파일
    추가

--- school.py
+++ school.py
@@ -1,12 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-import datetime
-
-present = str(datetime.datetime.now())
-print(present)
-date = present[:4] + present[5:7] + present[8:10] 
-print(date)
-
-req = requests.get("http://gsm.gen.hs.kr/xboard/board.php?tbnum=8")
-soup = BeautifulSoup(req.text, "html.parser")
-print(soup)
+import xml.etree.ElementTree as ET
+tree = ET.parse('')
