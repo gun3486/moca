2da2e9779e5468f4ea0aaf1d9d40dc2ec509ae4d
gun3486 <gun3486@naver.com>
2021-03-30 20:53:08 +0900

    코드수정
    코드수정

--- school.py
+++ school.py
@@ -1,7 +1,21 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('C:\Users\user\Documents\Github\moca\급식식단정보')
-root = tree.getroot()
-print(root.tag)
-for i in root:
-    print(i.tag, i.text)
-    
+import requests, re, json, sys
+
+try:
+    officeCode=sys.argv[1] ## 교육청 코드
+    schulCode=sys.argv[2] ## 학교 고유코드
+    schulCrseCode=sys.argv[3] ## 학교 분류코드 (고등학교, 중학교, 초등학교)
+    schulKndScCode="0" + str(sys.argv[3]) ## 학교 분류코드
+    schYm=sys.argv[4]
+except:
+    print("""!!ERROR.... 
+            코드 인자 값이 맞지 않거나 값이 이상합니다.
+            제대로 된 코드 값을 입력해주십쇼""")
+    sys.exit(1)
+
+
+URL="https://" + officeCode + "/sts_sci_md00_001.do"
+params = {'schulCode': str(schulCode), 'schulCrseScCode': str(schulCrseCode), 
+'schulKndScCode' : str(schulKndScCode), 
+'schYm' : str(schYm)}
+response = requests.get(URL, params=params).text
+data = response[response.find("<tbody>"):response.find("</tbody>")]
