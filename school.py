d929f2e9caddaca78a5f9a28c2b25b380d26a682
gun3486 <gun3486@naver.com>
2021-03-30 21:03:52 +0900

    코드 추가
    코드추가

--- school.py
+++ school.py
@@ -12,10 +12,33 @@ except:
             제대로 된 코드 값을 입력해주십쇼""")
     sys.exit(1)
 
-
 URL="https://" + officeCode + "/sts_sci_md00_001.do"
-params = {'schulCode': str(schulCode), 'schulCrseScCode': str(schulCrseCode), 
-'schulKndScCode' : str(schulKndScCode), 
-'schYm' : str(schYm)}
+params = {'schulCode': str(schulCode), 'schulCrseScCode': str(schulCrseCode), 'schulKndScCode' : str(schulKndScCode), 'schYm' : str(schYm)}
 response = requests.get(URL, params=params).text
 data = response[response.find("<tbody>"):response.find("</tbody>")]
+
+#불필요한 데이터 삭제
+regex = re.compile(r'[\n\r\t]')
+data=regex.sub('',data)
+rex = re.compile(r'<div>(.*?)</div>', re.S|re.M)
+data=rex.findall(data)
+
+file_json={}
+for dat in data:
+    date=re.findall(r"[0-3][0-9]",dat[0:2])
+    menu=dat[dat.find("[중식]<br />"):]
+    if not date:
+        date=dat[0:1]
+        if date == "" or date == " ":
+            continue
+    if type(date) == list:
+        date=date[0]
+    menu = menu.split("<br />")
+    menu.remove(menu[0])
+    if not menu:
+        menu="None"
+    file_json.update({date : menu})
+    
+##Json 생성
+with open('menu.json', 'w', encoding='utf-8') as outfile:
+       json.dump(file_json, outfile, sort_keys = False, indent=4, ensure_ascii=0)
